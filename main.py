03ce636f6574f5b648f3ad7a4ab548d8654f4a21
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-03-28 22:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+# Student Grade Predictor (Intermediate Beginner Version)
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
