44862360772f6ce6001b49c218748d69510f83a6
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-03-28 23:01:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,3 +44,14 @@ print(df.head())
 # -------------------------------
 print("\nDataset Info:\n")
 print(df.describe())
+
+# -------------------------------
+# 3. PREPARE DATA
+# -------------------------------
+X = df[["study_hours", "attendance", "sleep_hours", "previous_score"]]
+y = df["final_score"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
